7509d39adfce6ae8fa4c0bab87bcb99b6d9de80b
Rave <monsieurobscurite@gmail.com>
2018-04-19 17:00:06 +0200

    Probably working changing brightness

--- app/src/main/java/com/kruk/kruczek/bethespy/Spying.java
+++ app/src/main/java/com/kruk/kruczek/bethespy/Spying.java
@@ -1,9 +1,11 @@
 package com.kruk.kruczek.bethespy;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 
 
 public class Spying extends AppCompatActivity
@@ -24,25 +26,75 @@ public class Spying extends AppCompatActivity
         previousBrightnessLevel = getActualBrightnessLevel();
         previousBrightnessMode = getActualBrightnessMode();
 
+        checkCorrectnessOfPermission();
         setDarkness();
     }
 
     private int getActualBrightnessLevel()
     {
-        int brightnessValue = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+        int brightnessValue = 100;
+
+        try
+        {
+            brightnessValue = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+        }
+        catch (Exception error)
+        {
+            Log.e("Error", "Cannot access system brightness");
+            error.printStackTrace();
+        }
+
         return brightnessValue;
     }
 
     private int getActualBrightnessMode()
     {
-        int brightnessMode = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, 0);
+        int brightnessMode = 0;
+
+        try
+        {
+            brightnessMode = Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, 0);
+        }
+        catch (Exception error)
+        {
+            Log.e("Error", "Cannot access system brightness");
+            error.printStackTrace();
+        }
+
         return brightnessMode;
+    }
+
+    private void checkCorrectnessOfPermission()
+    {
+        if (!hasWriteSettingsPermission())
+            changeWriteSettingsPermission();
+    }
+
+    private boolean hasWriteSettingsPermission()
+    {
+        boolean permission = true;
+        permission = Settings.System.canWrite(context);
+        return permission;
+    }
+
+    private void changeWriteSettingsPermission()
+    {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        context.startActivity(intent);
     }
 
     private void setDarkness()
     {
-        Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
-        Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, DARKNESS);
+        try
+        {
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, DARKNESS);
+        }
+        catch (Exception error)
+        {
+            Log.e("Error", "Cannot access system brightness");
+            error.printStackTrace();
+        }
     }
 
     @Override
@@ -54,7 +106,45 @@ public class Spying extends AppCompatActivity
 
     private void setPreviousBrightness()
     {
-        Settings.System.getInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, previousBrightnessLevel);
+        try
+        {
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, previousBrightnessLevel);
+            Settings.System.putInt(context.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, previousBrightnessMode);
+        }
+        catch (Exception error)
+        {
+            Log.e("Error", "Cannot access system brightness");
+            error.printStackTrace();
+        }
     }
+
+    @Override
+    protected void onPause()
+    {
+        setPreviousBrightness();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        setPreviousBrightness();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        setDarkness();
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        setDarkness();
+        super.onStart();
+    }
+
 
 }
